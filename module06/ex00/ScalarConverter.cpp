602352fe1ea31737484f451dac497f8aa7e2cee9
Geraud t'Serstevens <geraudts@gmail.com>
2024-02-19 18:27:30 +0100

    checker + converter

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -6,67 +6,93 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:58:30 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/02/16 17:16:33 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/02/19 18:26:36 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
 #include <string>
-#include <cctype>
-#include <iomanip>
+#include <sstream>
+#include <limits>
 
-void	ScalarConverter::convert(std::string& input){
+bool	isChar(std::string& input)
+{
+	return (input.length() == 1 && std::isprint(input[0]));
+}
 
-	char	to_char = '\0';
-	int		to_int;
-	float	to_float;
-	double	to_double;
+bool	isInt(std::string& input)
+{
+	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length(); i++)
+		if (!std::isdigit(input[i]))
+			return (false);
+	return (true);
+}
 
-	if (input.length() == 1 && isalpha(input[0]))
-	{
-		to_char = input[0];
-		to_int = static_cast<int>(to_char);
-		to_float = static_cast<float>(to_char);
-		to_double = static_cast<double>(to_char);
-	}
+bool	isFloat(std::string& input)
+{
+	if (input.compare("-inff") || input.compare("+inff") || input.compare("nanf"))
+		return (true);
+	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length() - 1; i++)
+		if (!std::isdigit(input[i]) && !input[i] != '.')
+			return (false);
+	if (input[input.length()] != 'f')
+		return (false);
 	else
+		return (true);
+}
+
+bool	isDouble(std::string& input)
+{
+	if (input.compare("-inf") || input.compare("+inf") || input.compare("nan"))
+		return (true);
+	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length(); i++)
+		if (!std::isdigit(input[i]) && !input[i] != '.')
+			return (false);
+	return (true);
+}
+
+void	toChar(std::string& input)
+{
+	char	char_value = input[0];
+	int		int_value = static_cast<int>(char_value);
+	float	float_value = static_cast<float>(char_value);
+	double	double_value = static_cast<double>(char_value);
+	std::cout << char_value << std::endl;
+	std::cout << int_value << std::endl;
+	std::cout << float_value << "f" << std::endl;
+	std::cout << double_value << std::endl;
+}
+
+void	toInt(std::string& input)
+{
+	int	int_value;
+
+	try
 	{
-		if (input.find('.') != std::string::npos)
-		{
-			if (input.find('f') != std::string::npos)
-			{
-				to_float = std::stof(input);
-				to_int = static_cast<int>(to_float);
-				to_double = static_cast<double>(to_float);
-			}
-			else
-			{
-				to_double = std::stod(input);
-				to_int = static_cast<int>(to_double);
-				to_float = static_cast<float>(to_double);
-			}
-		}
-		else
-		{
-			if (input.length() > 9)
-			{
-				std::cout << "Type conversion is impossible" << std::endl;
-				return;
-			}
-			else
-			{
-				to_int = stoi(input);
-				to_float = static_cast<float>(to_int);
-				to_double = static_cast<double>(to_int);
-			}
-		}
+		int_value = stoi(input);
+		std::cout << int_value << std::endl;
 	}
-	if (to_char == '\0')
-		std::cout << "char: Non displayable" << std::endl;
-	else
-		std::cout << "char: " << to_char << std::endl;
-	std::cout << "int: " << to_int << std::endl;
-	std::cout << "float: " << std::fixed << std::setprecision(input.size() - input.find('.') - 1) << to_float << "f" << std::endl;
-	std::cout << "double: " << to_double << std::endl;
+	catch (const std::exception& e)
+	{
+		std::cout << "int: impossible" << std::endl;
+	}
+	char	char_value = static_cast<char>(int_value);
+	float	float_value = static_cast<float>(int_value);
+	double	double_value = static_cast<double>(int_value);
+	std::cout << char_value << std::endl;
+	std::cout << float_value << "f" << std::endl;
+	std::cout << double_value << std::endl;
+}
+
+void	ScalarConverter::convert(std::string& input)
+{
+	if (isChar(input))
+		toChar(input);
+	else if (isInt(input))
+		toInt(input);
+	else if (isFloat(input))
+		toFloat(input);
+	else if (isDouble(input))
+		toDouble(input);
 }
--- module06/ex00/ScalarConverter.hpp
+++ module06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:53:38 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/02/16 15:58:49 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/02/19 18:09:18 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,10 @@ class ScalarConverter{
 	public:
 		static void	convert(std::string& input);
 };
+
+bool	isChar(std::string& input);
+bool	isInt(std::string& input);
+bool	isFloat(std::string& input);
+bool	isDouble(std::string& input);
 
 #endif
