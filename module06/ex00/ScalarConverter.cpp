0c7495d70d430576e854896b88e58a6595bc0af5
Geraud t'Serstevens <geraudts@gmail.com>
2024-02-19 20:50:51 +0100

    converter first version

--- module06/ex00/ScalarConverter.cpp
+++ module06/ex00/ScalarConverter.cpp
@@ -6,72 +6,79 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 10:58:30 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/02/19 18:26:36 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/02/19 20:45:47 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
 #include <string>
-#include <sstream>
 #include <limits>
 
-bool	isChar(std::string& input)
+static bool	isChar(std::string& input)
 {
-	return (input.length() == 1 && std::isprint(input[0]));
+	return (input.length() == 1 && std::isprint(input[0]) && !std::isdigit(input[0]));
 }
 
-bool	isInt(std::string& input)
+static bool	isInt(std::string& input)
 {
 	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length(); i++)
 		if (!std::isdigit(input[i]))
 			return (false);
+	try
+	{
+		stoi(input);
+	}
+	catch(const std::exception& e)
+	{
+		return (false);
+	}
 	return (true);
 }
 
-bool	isFloat(std::string& input)
+static bool	isFloat(std::string& input)
 {
-	if (input.compare("-inff") || input.compare("+inff") || input.compare("nanf"))
+	if (!input.compare("-inff") || !input.compare("+inff") || !input.compare("nanf"))
 		return (true);
 	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length() - 1; i++)
-		if (!std::isdigit(input[i]) && !input[i] != '.')
+		if (!std::isdigit(input[i]) && input[i] != '.')
 			return (false);
-	if (input[input.length()] != 'f')
+	if (input[input.length() - 1] != 'f')
 		return (false);
 	else
 		return (true);
 }
 
-bool	isDouble(std::string& input)
+static bool	isDouble(std::string& input)
 {
-	if (input.compare("-inf") || input.compare("+inf") || input.compare("nan"))
+	if (!input.compare("-inf") || !input.compare("+inf") || !input.compare("nan"))
 		return (true);
 	for (size_t i = (input.at(0) == '-' ? 1 : 0); i < input.length(); i++)
-		if (!std::isdigit(input[i]) && !input[i] != '.')
+		if (!std::isdigit(input[i]) && input[i] != '.')
 			return (false);
 	return (true);
 }
 
-void	toChar(std::string& input)
+static void	toChar(std::string& input)
 {
 	char	char_value = input[0];
 	int		int_value = static_cast<int>(char_value);
 	float	float_value = static_cast<float>(char_value);
 	double	double_value = static_cast<double>(char_value);
-	std::cout << char_value << std::endl;
-	std::cout << int_value << std::endl;
-	std::cout << float_value << "f" << std::endl;
-	std::cout << double_value << std::endl;
+	std::cout << "char: " << char_value << std::endl;
+	std::cout << "int: " << int_value << std::endl;
+	std::cout << "float: " << float_value << "f" << std::endl;
+	std::cout << "double: " << double_value << std::endl;
 }
 
-void	toInt(std::string& input)
+static void	toInt(std::string& input)
 {
 	int	int_value;
 
 	try
 	{
 		int_value = stoi(input);
-		std::cout << int_value << std::endl;
+		std::cout << "int: " << int_value << std::endl;
 	}
 	catch (const std::exception& e)
 	{
@@ -80,9 +87,71 @@ void	toInt(std::string& input)
 	char	char_value = static_cast<char>(int_value);
 	float	float_value = static_cast<float>(int_value);
 	double	double_value = static_cast<double>(int_value);
-	std::cout << char_value << std::endl;
-	std::cout << float_value << "f" << std::endl;
-	std::cout << double_value << std::endl;
+	std::cout << "char: ";
+	if (!std::isprint(char_value))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << char_value << std::endl;
+	std::cout << "float: " << float_value << "f" << std::endl;
+	std::cout << "double: " << double_value << std::endl;
+}
+
+static void	toFloat(std::string& input)
+{
+	float	float_value;
+
+	try
+	{
+		float_value = stof(input);
+		std::cout << "float: " << float_value << "f" << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "float: impossible" << std::endl;
+	}
+	char	char_value = static_cast<char>(float_value);
+	int		int_value = static_cast<int>(float_value);
+	double	double_value = static_cast<double>(float_value);
+	std::cout << "char: ";
+	if (!std::isprint(char_value))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << char_value << std::endl;
+	std::cout << "int: ";
+	if (int_value == std::numeric_limits<int>::min())
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << int_value << std::endl;
+	std::cout << "double: " << double_value << std::endl;
+}
+
+static void	toDouble(std::string& input)
+{
+	double	double_value;
+
+	try
+	{
+		double_value = stod(input);
+		std::cout << "double: " << double_value << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "double: impossible" << std::endl;
+	}
+	char	char_value = static_cast<char>(double_value);
+	int		int_value = static_cast<int>(double_value);
+	float	float_value = static_cast<float>(double_value);
+	std::cout << "char: ";
+	if (!std::isprint(char_value))
+		std::cout << "Non displayable" << std::endl;
+	else
+		std::cout << char_value << std::endl;
+	std::cout << "int: ";
+	if (int_value == std::numeric_limits<int>::min())
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << int_value << std::endl;
+	std::cout << "float: " << float_value << "f" << std::endl;
 }
 
 void	ScalarConverter::convert(std::string& input)
--- module06/ex00/main.cpp
+++ module06/ex00/main.cpp
@@ -3,15 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gt-serst <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 11:12:46 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/02/16 12:00:15 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/02/19 20:50:06 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-#include <iostream>
 
 int	main(int ac, char **av)
 {
