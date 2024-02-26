3b0d9589dc19074574685fecc8070a18c5c2939b
Geraud t'Serstevens <geraudts@gmail.com>
2024-02-26 20:52:56 +0100

    module07 done

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gt-serst <gt-serst@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/23 13:54:06 by gt-serst          #+#    #+#             */
-/*   Updated: 2024/02/23 17:32:49 by gt-serst         ###   ########.fr       */
+/*   Updated: 2024/02/26 19:26:49 by gt-serst         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,19 @@
 
 int	main(void)
 {
-	Array<int> int_array(10);
-	//Array<std::string> string_array(20);
+	try
+	{
+		Array<int> default_array;
+		Array<int> int_array(10);
+		Array<std::string> string_array(10);
 
-	std::cout << int_array.size() << std::endl;
-	//std::cout << string_array.size() << std::endl;
+		std::cout << default_array.size() << std::endl;
+		std::cout << int_array.size() << std::endl;
+		std::cout << string_array.size() << std::endl;
+	}
+	catch (Array<int>::IndexOutOfBoundsException& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 	return (0);
 }
